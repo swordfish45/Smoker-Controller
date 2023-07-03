4f4241d670adc88664b016d6b6679f91fb1b875f
Sam Marlin <swordfish45@gmail.com>
2023-07-03 10:53:35 -0400

    Support for grafana and influxdb

--- control.py
+++ control.py
@@ -7,60 +7,121 @@ import math
 import sqlite3
 import re
 import set_shutter
-
-GPIO.setmode(GPIO.BCM)
-DEBUG = 1
-
-#targettemp = 240
-PWMPIN = 13
-
-path='/home/pi/Smoker-Controller'
-
-def readAperture():
-    str = open(path+'/web/formdata','r').read()
-    match = re.search('\"apertureset\":\"(\d+)',str)
-    if (DEBUG == 1):
-        print int(match.group(1))
-    return int(match.group(1))
-
-def readSetPoint():
-    str = open(path+'/web/formdata','r').read()
-    match = re.search('\"sensorset\":\"(\d+)',str)
-    return int(match.group(1))
-
-# Set PWM of fan output port based on current steady state and target temp F
-def setFan(temp, target):
-    if (DEBUG == 1):
-        print temp, target
-    if (temp < target):
-        GPIO.output(PWMPIN, True)
-    else:
-        GPIO.output(PWMPIN, False)
+from influxdb import InfluxDBClient
 
 
-# Get steady state reading of the smokebox temp
-def readTemp():
-	
-    conn=sqlite3.connect(path+'/templog.db')
-    curs=conn.cursor()
-    curs.execute("SELECT max(timestamp), sensnum, temp FROM (SELECT * FROM temps WHERE sensnum = 0)")
-    temp = curs.fetchone()[2]
-    if (DEBUG == 1):
-      print "db", temp
+class control:
 
-    conn.close()
-    setFan(temp,readSetPoint())
-    set_shutter.setAperture(readAperture())
-def testloop():
-	while True:
-		readTemp()
-	        GPIO.output(PWMPIN,True)
-	        time.sleep(1)
-	        GPIO.output(PWMPIN,False)
-        	time.sleep(1)
+    def readAperture(self):
+        str = open(self.path+'/web/formdata','r').read()
+        match = re.search('\"apertureset\":\"(\d+)',str)
 
-GPIO.setup(PWMPIN,GPIO.OUT)
-#testloop()
-readTemp()
-readAperture()
-#GPIO.cleanup()
+        choke = int(match.group(1))
+        if (self.DEBUG == 1):
+            print choke
+            
+        measurement = 'choke'
+        fields = {'value': choke}
+
+        # Create an InfluxDB measurement
+        data = [
+            {
+                'measurement': measurement,
+                'fields': fields
+            }
+        ]
+
+        # Write the data to InfluxDB
+        self.client.write_points(data)
+     
+        return choke
+
+    def readSetPoint(self):
+        str = open(self.path+'/web/formdata','r').read()
+        match = re.search('\"sensorset\":\"(\d+)',str)
+        setpoint = int(match.group(1))
+
+        measurement = 'setpoint'
+        fields = {'value': setpoint}
+
+        # Create an InfluxDB measurement
+        data = [
+            {
+                'measurement': measurement,
+                'fields': fields
+            }
+        ]
+
+        # Write the data to InfluxDB
+        self.client.write_points(data)
+
+        return setpoint
+
+    # Set PWM of fan output port based on current steady state and target temp F
+    def setFan(self, temp, target):
+        if (self.DEBUG == 1):
+            print temp, target
+
+        fan_enable = temp < target
+
+        GPIO.output(self.PWMPIN, fan_enable)
+
+        measurement = 'fan_enable'
+        fields = {'value': fan_enable}
+
+        # Create an InfluxDB measurement
+        data = [
+            {
+                'measurement': measurement,
+                'fields': fields
+            }
+        ]
+
+        # Write the data to InfluxDB
+        self.client.write_points(data)
+
+
+    # Get steady state reading of the smokebox temp
+    def readTemp(self):
+        
+        conn=sqlite3.connect(self.path+'/templog.db')
+        curs=conn.cursor()
+        curs.execute("SELECT max(timestamp), sensnum, temp FROM (SELECT * FROM temps WHERE sensnum = 0)")
+        temp = curs.fetchone()[2]
+        if (self.DEBUG == 1):
+            print "db", temp
+
+        conn.close()
+        self.setFan(temp,self.readSetPoint())
+        set_shutter.setAperture(self.readAperture())
+
+    def testloop(self):
+        while True:
+            self.readTemp()
+            GPIO.output(self.PWMPIN,True)
+            time.sleep(1)
+            GPIO.output(self.PWMPIN,False)
+            time.sleep(1)
+
+    def __init__(self):
+        self.client = InfluxDBClient(host='localhost', port=8086)
+        self.db_name = 'tempcontroler'
+        self.client.create_database(self.db_name)
+        self.client.switch_database(self.db_name)
+
+        GPIO.setmode(GPIO.BCM)
+        self.DEBUG = 1
+
+        #targettemp = 240
+        self.PWMPIN = 13
+
+        self.path='/home/pi/Smoker-Controller'
+
+        GPIO.setup(self.PWMPIN,GPIO.OUT)
+        #testloop()
+        self.readTemp()
+        self.readAperture()
+        #GPIO.cleanup()
+
+if __name__ == "__main__":
+    control()
--- logger.py
+++ logger.py
@@ -5,12 +5,11 @@ import os
 import RPi.GPIO as GPIO
 import math
 import sqlite3
+from influxdb import InfluxDBClient
 
-GPIO.setmode(GPIO.BCM)
-DEBUG = 0
-
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+class logger:
+    # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+    def readadc(self, adcnum, clockpin, mosipin, misopin, cspin):
         if ((adcnum > 7) or (adcnum < 0)):
                 return -1
         GPIO.output(cspin, True)
@@ -22,105 +21,140 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         commandout |= 0x18  # start bit + single-ended bit
         commandout <<= 3    # we only need to send 5 bits here
         for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+            if (commandout & 0x80):
+                    GPIO.output(mosipin, True)
+            else:
+                    GPIO.output(mosipin, False)
+            commandout <<= 1
+            GPIO.output(clockpin, True)
+            GPIO.output(clockpin, False)
 
         adcout = 0
         # read in one empty bit, one null bit and 10 ADC bits
         for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+            GPIO.output(clockpin, True)
+            GPIO.output(clockpin, False)
+            adcout <<= 1
+            if (GPIO.input(misopin)):
+                    adcout |= 0x1
 
         GPIO.output(cspin, True)
         
         adcout >>= 1       # first bit is 'null' so drop it
         return adcout
 
-def temp_calc(value):
-    volts = (value * 3.3) / 1024 #calculate the voltage
-    ohms = ((1/volts)*3300)-1000 #calculate the ohms of the thermististor
+    def temp_calc(self, value):
+        volts = (value * 3.3) / 1024 #calculate the voltage
+        ohms = ((1/volts)*3300)-1000 #calculate the ohms of the thermististor
 
-    lnohm = math.log1p(ohms) #take ln(ohms)
+        lnohm = math.log1p(ohms) #take ln(ohms)
 
-    #a, b, & c values from http://www.thermistor.com/calculators.php
-    #using curve R (-6.2%/C @ 25C) Mil Ratio X
-    #a =  0.002197222470870
-    #b =  0.000161097632222
-    #c =  0.000000125008328
+        #a, b, & c values from http://www.thermistor.com/calculators.php
+        #using curve R (-6.2%/C @ 25C) Mil Ratio X
+        #a =  0.002197222470870
+        #b =  0.000161097632222
+        #c =  0.000000125008328
 
-    a =  0.000570569668444 
-    b =  0.000239344111326 
-    c =  0.000000047282773 
+        a =  0.000570569668444 
+        b =  0.000239344111326 
+        c =  0.000000047282773 
 
-    #Steinhart Hart Equation
-    # T = 1/(a + b[ln(ohm)] + c[ln(ohm)]^3)
+        #Steinhart Hart Equation
+        # T = 1/(a + b[ln(ohm)] + c[ln(ohm)]^3)
 
-    t1 = (b*lnohm) # b[ln(ohm)]
+        t1 = (b*lnohm) # b[ln(ohm)]
 
-    c2 = c*lnohm # c[ln(ohm)]
+        c2 = c*lnohm # c[ln(ohm)]
 
-    t2 = math.pow(c2,3) # c[ln(ohm)]^3
+        t2 = math.pow(c2,3) # c[ln(ohm)]^3
 
-    temp = 1/(a + t1 + t2) #calcualte temperature
+        temp = 1/(a + t1 + t2) #calcualte temperature
 
-    tempc = temp - 273.15 - 4 #K to C
+        tempc = temp - 273.15 - 4 #K to C
 
-    tempf = tempc*9/5 + 32
-    # the -4 is error correction for bad python math
+        tempf = tempc*9/5 + 32
+        # the -4 is error correction for bad python math
 
-    #print out info
-    #print ("%4d/1023 => %5.3f V => %4.1f ohms  => %4.1f K => %4.1f C  => %4.1f F" % (value, volts, ohms, temp, tempc, tempf))
+        #print out info
+        #print ("%4d/1023 => %5.3f V => %4.1f ohms  => %4.1f K => %4.1f C  => %4.1f F" % (value, volts, ohms, temp, tempc, tempf))
 
-    return tempf
+        return tempf
     
-def log_temperature(sensnum,temp):
+    def log_temperature(self,sensnum,temp):
 
-    conn=sqlite3.connect('/home/pi/Smoker-Controller/templog.db')
-    curs=conn.cursor()
-    curs.execute("INSERT INTO temps (sensnum,temp) values((?), (?))", (sensnum,temp,))
+        conn=sqlite3.connect('/home/pi/Smoker-Controller/templog.db')
+        curs=conn.cursor()
+        curs.execute("INSERT INTO temps (sensnum,temp) values((?), (?))", (sensnum,temp,))
 
-    # commit the changes
-    conn.commit()
+        # commit the changes
+        conn.commit()
 
-    conn.close()
+        conn.close()
+
+    def log_temperature_influx(self,sensnum,temp):
+        # Define the temperature data
+        measurement = 'temperature'
+        tags = {'sensor': sensnum}
+        fields = {'value': temp}
+
+        # Create an InfluxDB measurement
+        data = [
+            {
+                'measurement': measurement,
+                'tags': tags,
+                'fields': fields
+            }
+        ]
+
+        # Write the data to InfluxDB
+        self.client.write_points(data)
 
 
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
-SPICLK = 18
-SPIMISO = 23
-SPIMOSI = 24
-SPICS = 25
+    def __init__(self):
+
+        GPIO.setmode(GPIO.BCM)
+        self.DEBUG = 0
+
+        self.client = InfluxDBClient(host='localhost', port=8086)
+        self.db_name = 'tempcontroler'
+        self.client.create_database(self.db_name)
+        self.client.switch_database(self.db_name)
 
 
-# set up the SPI interface pins
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
 
-for sensor in xrange(0,3):
-		value = 0
-		temp = 0 
-		value = readadc(sensor, SPICLK, SPIMOSI, SPIMISO, SPICS)
-		if value != 0:
-			temp = temp_calc(value)
-			temp = int(temp)
-			if DEBUG:
-				print "value:", value, sensor
-			log_temperature(sensor, temp)
-		#log temperature of 0 if sensor is not connected
-		if value == 0:
-			if DEBUG:
-				print "value:", value, sensor
-			log_temperature(sensor, 0)
+        # change these as desired - they're the pins connected from the
+        # SPI port on the ADC to the Cobbler
+        SPICLK = 18
+        SPIMISO = 23
+        SPIMOSI = 24
+        SPICS = 25
 
-GPIO.cleanup()
+
+        # set up the SPI interface pins
+        GPIO.setup(SPIMOSI, GPIO.OUT)
+        GPIO.setup(SPIMISO, GPIO.IN)
+        GPIO.setup(SPICLK, GPIO.OUT)
+        GPIO.setup(SPICS, GPIO.OUT)
+
+        for sensor in xrange(0,3):
+            value = 0
+            temp = 0 
+            value = self.readadc(sensor, SPICLK, SPIMOSI, SPIMISO, SPICS)
+            if value != 0:
+                temp = self.temp_calc(value)
+                if self.DEBUG:
+                    print "value:", value, sensor
+                self.log_temperature(sensor, int(temp))
+                self.log_temperature_influx(sensor, temp)
+            #log temperature of 0 if sensor is not connected
+            if value == 0:
+                if self.DEBUG:
+                    print "value:", value, sensor
+                self.log_temperature(sensor, int(0))
+                self.log_temperature_influx(sensor, 0)
+
+
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    logger()
